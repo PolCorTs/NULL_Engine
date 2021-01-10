64a39e777dc656df039fc535815bedccdab3f81b
Pol Cortés Escañuela <36264888+PolCorTs@users.noreply.github.com>
2021-01-10 20:05:10 +0100

    Updated audio modules (not working)

--- NULL Engine/Source/M_Audio.h
+++ NULL Engine/Source/M_Audio.h
@@ -1,8 +1,40 @@
-#ifndef __M_Audio_H__
-#define __M_Audio_H__
+#ifndef __M_Audio__H__
+#define __M_Audio__H__
 
-#include <vector>
 #include "Module.h"
+#include "glmath.h"
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+class WwiseGameObject
+{
+	WwiseGameObject(unsigned __int64 id, const char* name);
+	~WwiseGameObject();
+
+public:
+	void SetPosition(float posX = 0, float posY = 0, float posZ = 0, float frontX = 1, float frontY = 0, float frontZ = 0, float topX = 0, float topY = 1, float topZ = 0);
+	void PlayEvent(uint id);
+	void PauseEvent(uint id);
+	void ResumeEvent(uint id);
+	void StopEvent(uint id);
+	void SetVolume(uint id, float volume);
+	WwiseGameObject* CreateAudioSource(uint id, const char* name, vec3 position);
+	WwiseGameObject* CreateAudioListener(uint id, const char* name, vec3 position);
+	void SetAuxSends();
+
+public:
+	uint GetID();
+
+public:
+	float volume = 1.0f;
+
+private:
+	const char* name = nullptr;
+	AkGameObjectID id;
+
+	AkVector position = { 0,0,0 };
+	AkVector orientationFront = { 0,0,0 };
+	AkVector orientationTop = { 0,0,0 };
+};
 
 class M_Audio : public Module
 {
@@ -10,9 +42,22 @@ public:
 	M_Audio(bool start_enabled = true);
 	virtual ~M_Audio();
 
+	bool Init(json file);
+	bool Start();
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+	void Tests(AkGameObjectID id);
+
 private:
 
-	bool InitWwise();
+	void InitWwise();
+	void TerminateWwise();
+	void LoadSoundBank(const char* path);
+
+public:
+	AkGameObjectID currentListenerID;
+	unsigned long g_envMAP[255];
+
 };
 
-#endif // !__M_Audio_H__
+#endif __M_Audio__H__
